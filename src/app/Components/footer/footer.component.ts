2feb1d58e8a39f5d6bc34f852140a499e5ee7019
Gabriel Duah <gabriel.duah@amalitech.com>
2024-04-22 11:18:41 +0200

    footer fetching done

--- src/app/Components/footer/footer.component.ts
+++ src/app/Components/footer/footer.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Jobs } from 'src/app/types';
+import { ActivatedRoute } from '@angular/router';
+import { JobsService } from 'src/app/service/jobs.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.css']
 })
-export class FooterComponent {
+export class FooterComponent implements OnInit {
+  
+  jobDetail$!:Observable<Jobs>
 
+  constructor(private jobService: JobsService, private route:ActivatedRoute) {}
+
+  ngOnInit(): void {
+    this.route.params.subscribe((params) => {
+      const jobId = params['id'];
+      this.jobDetail$ = this.jobService.getJobById(jobId)
+      console.log("job Id:", jobId)
+    })
+  }
 }

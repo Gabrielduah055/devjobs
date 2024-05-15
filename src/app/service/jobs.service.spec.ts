afd83e297be1bf6fb89d09fc505a19e01e9196f1
Gabriel Duah <gabriel.duah@amalitech.com>
2024-05-15 16:29:02 +0200

    testing for the jobs and error handling is done

--- src/app/service/jobs.service.spec.ts
+++ src/app/service/jobs.service.spec.ts
@@ -8,7 +8,7 @@ import { JobsService } from './jobs.service';
 
 describe('JobsService', () => {
   let service: JobsService;
-  let HttpClient:HttpClient;
+  let httpClient:HttpClient;
   let httpTestingController: HttpTestingController;
 
   beforeEach(() => {
@@ -17,9 +17,62 @@ describe('JobsService', () => {
       providers:[JobsService]
     });
     service = TestBed.inject(JobsService);
+    httpClient = TestBed.inject(HttpClient);
+    httpTestingController = TestBed.inject(HttpTestingController)
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  it('should retura an observable<JObs>', () => {
+    const mockJobs: Jobs[] = [
+      {
+        id: 1, 
+        position: 'job 1', 
+        description: 'Description 1',
+        company: 'company',
+        logo: 'logo2.png',
+        logoBackground: 'background2.png',
+        postedAt: 'position 2',
+        contract: '2024-05-16',
+        location: 'location 2',
+        website: 'website',
+        apply: 'apply',
+        requirements: {
+          content:'requirment content',
+          items:['requirement item1', 'requirement item 2']
+        },
+        role: {
+          content: 'role 1',
+          items:['role 2', 'role 3']
+        }
+      },
+    ]
+
+    service.getJobs().subscribe(jobs => {
+      expect(jobs.length).toBe(1)
+      expect(jobs).toEqual(mockJobs)
+    });
+
+    const req = httpTestingController.expectOne('https://64281ee346fd35eb7c4bfc31.mockapi.io/dev')
+    expect(req.request.method).toBe('GET');
+
+    req.flush(mockJobs)
+  });
+
+  it('should handle errors properly', () => {
+    const errorMessage = '404 Not Found';
+
+    service.getJobs().subscribe(
+      () => {},
+      error => {
+        expect(error).toBeTruthy();
+        expect(error).toBe('Something went wrong while fetching jobs data. Please try again later.')
+      }
+    );
+
+    const req = httpTestingController.expectOne('https://64281ee346fd35eb7c4bfc31.mockapi.io/dev');
+    req.error(new ErrorEvent(errorMessage), {status:404});
+  })
 });
